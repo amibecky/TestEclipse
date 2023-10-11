8cca459a151b2b8feb49238b2425750fd3715b0e
amibecky <snips_ami@msn.com>
2023-10-11 13:17:55 +0900

    新規プロジェクト作成

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!!");
+	//eclipsはプロジェクトごとに、リモートリポジトリにいれられる。testGit　は移動するフォルダだから、リモートリポジトリをいれてはだめ
+	//プロジェクトの中以外につくるべき
+	
 	}
 
 }
